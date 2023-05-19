68a855e6c97e2e2570fa4791a9ec5a6dfc2bdf9d
airtoour <artur.gips@mail.ru>
2023-05-19 22:53:47 +0700

    почти работает, совсем чуток

--- РГР/RGR-RPP.py
+++ РГР/RGR-RPP.py
@@ -1,33 +1,36 @@
-import requests, json
+import requests,  json
 import threading, time
 import logging
-import numpy as np
+import numpy    as np
 import psycopg2 as pg
 
 from aiogram                            import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state   import State, StatesGroup
 from aiogram.dispatcher                 import FSMContext
-from aiogram.types                      import ParseMode
+from aiogram.types                      import ParseMode, KeyboardButton, ReplyKeyboardMarkup
 
 API_KEY = "61HMKMI43NOMJFEO"
 TOKEN = "6165848339:AAE2sRqeBVZ7ss23Kw9J9zIx-0I7I5mWu5c"
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token = TOKEN)
-dp = Dispatcher(bot, storage = MemoryStorage())
+dp  = Dispatcher(bot, storage = MemoryStorage())
 
 # Создание списка кнопок
 buttons = [
-    types.InlineKeyboardButton(text = 'Добавить ценную бумагу',  callback_data = 'add_security'),
-    types.InlineKeyboardButton(text = 'Показатели ценных бумаг', callback_data = 'save_cur'),
-    types.InlineKeyboardButton(text = 'Медиана цены закрытия',   callback_data = 'median')
+    [
+        KeyboardButton(text = "Добавить ценную бумагу"),
+        KeyboardButton(text = "Показатели ценных бумаг"),
+        KeyboardButton(text = "Медиана цены закрытия"),
+    ]
 ]
 
-# Создание объекта класса InlineKeyboardMarkup с передачей списка кнопок в конструктор
-keyboard = types.InlineKeyboardMarkup(row_width = 3)
-keyboard.add(*buttons)
-
+keyboard = ReplyKeyboardMarkup(
+    keyboard          = buttons,
+    resize_keyboard   = True,
+    one_time_keyboard = True
+)
 
 # Функция для подключения к базе данных
 def connect_db():
@@ -45,14 +48,13 @@ class States(StatesGroup):
 # Обработчик для команды /start
 @dp.message_handler(commands = ['start'])
 async def start_handler(message: types.Message):
-    await message.answer(f"""Привет! Я бот для расчета ценных бумаг. Для начала, выбери действия ниже.
+    await message.answer( """Привет! Я бот для расчета ценных бумаг. Для начала, выбери действия ниже.
                              Действия должны быть в порядке:
-                             /start,
-                             Кнопка 'Добавить ценную бумагу',
-                             Кнопка 'Показатели ценных бумаг',
-                             Кнопка 'Медиана цены закрытия'!
-                          """)
-    await message.answer("Выберите действие:", reply_markup = keyboard)
+                             1. /start,
+                             2. Кнопка 'Добавить ценную бумагу',
+                             3. Кнопка 'Показатели ценных бумаг',
+                             4. Кнопка 'Медиана цены закрытия'!""")
+    await message.answer("Выберите действие:", reply_markup=keyboard)
 
 
 # Обработчик для команды /help
@@ -62,19 +64,16 @@ async def help_handler(message: types.Message):
 
 
 # Обработчик для команды "Добавить ценную бумагу к портфелю"
-@dp.message_handler(lambda c: c.data == 'add_security')
+@dp.message_handler(lambda message: message.text == 'Добавить ценную бумагу')
 async def add_security(message: types.Message):
-
     # Отправляем пользователю сообщение с просьбой ввести имя ценной бумаги
-    await message.reply("Введите имя ценной бумаги:")
-
-    # Сохраняем состояние пользователя в хранилище FSM
+    await message.answer("Введите имя ценной бумаги:")
     await States.add_security.set()
 
-# Обработчик для получения имени ценной бумаги после команды "Добавить ценную бумагу к портфелю"
-@dp.message_handler(state = States.add_security)
-async def save_security(message: types.Message, state: FSMContext):
 
+# Обработчик для получения имени ценной бумаги после команды "Добавить ценную бумагу к портфелю"
+@dp.message_handler(state=States.add_security)
+async def save_security(message: types.Message, state: FSMContext):
     # Получаем имя ценной бумаги, введенное пользователем
     security_name = message.text
 
@@ -90,8 +89,8 @@ async def save_security(message: types.Message, state: FSMContext):
     await state.finish()
 
 
-# Обработчик для Индикаторов
-@dp.message_handler(lambda c: c.data == 'show_indicators')
+# Обработчик для команды "Показатели ценных бумаг"
+@dp.message_handler(lambda message: message.text == 'Показатели ценных бумаг')
 async def show_indicators(message: types.Message):
     # Получаем показатели из базы данных для данного пользователя
     conn = connect_db()
@@ -105,7 +104,7 @@ async def show_indicators(message: types.Message):
         for security in securities:
             security_name = security[0]
             # Получаем показатели для ценной бумаги из базы данных
-            conn = connect_db()
+            conn   = connect_db()
             cursor = conn.cursor()
             cursor.execute("select indicator_value from indicators where security_name = %s",
                            (security_name,))
@@ -114,47 +113,35 @@ async def show_indicators(message: types.Message):
 
             if indicators:
                 # Отправляем показатели пользователю
-                await message.answer(f"Показатели для ценной бумаги {security_name}:", reply_markup = keyboard)
+                await message.answer(f"Показатели для ценной бумаги {security_name}:", reply_markup=keyboard)
                 for indicator in indicators:
                     await message.answer(f"- {indicator[0]}")
             else:
                 await message.answer(f"Для ценной бумаги {security_name} не найдено значений!")
     else:
-        await message.answer("У вас нет отслеживаемых ценных бумаг :( Проверьте их наличие в Базе данных!")
+        await message.answer("У вас нет отслеживаемых ценных бумаг. Проверьте их наличие в Базе данных!")
 
 
 # Определение показателя доходности ценной бумаги
-def avg_close_sum(security_name):
-    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={security_name}&apikey={API_KEY}"
-    response = requests.get(url)
-    data = json.loads(response.text)
+def calculate_and_store_data(security_name):
+    url          = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={security_name}&apikey={API_KEY}"
+    response     = requests.get(url)
+    data         = json.loads(response.text)
 
-    series = data['Time Series (Daily)']
+    series       = data['Time Series (Daily)']
     close_prices = []
 
     for date, values in series.items():
         close_price = float(values['4. close'])
         close_prices.append(close_price)
 
-    returns = np.diff(close_prices)/close_prices[:-1]
+    returns         = np.diff(close_prices) / close_prices[:-1]
     indicator_value = np.mean(returns)
 
     return indicator_value
 
+
 def task():
-    blanks = []
-    while True:
-        for blank in blanks:
-            avg_close_sum(blank)
-
-            ts = time.time()
-            # Store the time value in the database
-        time.sleep(10)
-
-# Start the task
-task()
-
-def calculate_indicators_task():
     while True:
         # Получаем список ценных бумаг из базы данных
         conn = connect_db()
@@ -168,7 +155,7 @@ def calculate_indicators_task():
                 security_name = security[0]
 
                 # Вызываем функцию для расчета показателя доходности
-                indicator_value = avg_close_sum(security_name)
+                indicator_value = calculate_and_store_data(security_name)
 
                 # Сохраняем показатель доходности в базу данных
                 conn = connect_db()
@@ -181,26 +168,30 @@ def calculate_indicators_task():
         # Приостанавливаем выполнение на 1 день
         time.sleep(24 * 60 * 60)
 
-@dp.message_handler(lambda c: c.data == 'median')
+
+# Запуск задачи в отдельном потоке
+thread = threading.Thread(target = task)
+thread.start()
+
+
+# Обработчик для команды "Медиана цены закрытия"
+@dp.message_handler(lambda message: message.text == 'Медиана цены закрытия')
 async def median_handler(message: types.Message):
-    
-    
-    # загрузка данных и вычисление медианы
-    conn = connect_db()
+    # Получаем данные и вычисляем медиану
+    conn   = connect_db()
     cursor = conn.cursor()
     cursor.execute("select indicator_value from indicators")
-    rows = cursor.fetchall()
+    rows   = cursor.fetchall()
     conn.close()
 
-    df = np.array(rows)
-    close_prices = df['Close'][-30:]
-    median_price = np.median(close_prices)
-    
-    # отправка ответа
+    indicator_values = [row[0] for row in rows]
+    median_price     = np.median(indicator_values)
+
+    # Отправляем ответ
     response = f"Медианная цена закрытия за последние 30 дней: {median_price}"
-    await message.answer(message.chat.id, response, parse_mode = ParseMode.HTML)
+    await message.answer(response, parse_mode = ParseMode.HTML)
+
 
 if __name__ == '__main__':
     thread = threading.Thread(target = task)
-    # Запускаем экземпляр 'thread'
     executor.start_polling(dp, skip_updates = True)
--- РГР/скрипт создания таблиц.sql
+++ РГР/скрипт создания таблиц.sql
@@ -1,11 +1,9 @@
-create table securities(
-	user_id serial primary key,
-	security_name varchar(255) unique
-);
-
 create table indicators(
-	security_name varchar(255) unique,
-	indicator_value numeric 
+	security_name   varchar(255),
+	indicator_value numeric
 );
 
-alter table securities add constraint security_name foreign key (security_name) references indicators(security_name);
+create table securities(
+	user_id       serial primary key,
+	security_name varchar(255)
+);
