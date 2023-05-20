e135c4903f41de95da290a0f3ee55bf0d1bfa469
airtoour <artur.gips@mail.ru>
2023-05-20 09:07:39 +0700

    почти почти почти почти почти почти почти.....

--- РГР/RGR-RPP.py
+++ РГР/RGR-RPP.py
@@ -1,5 +1,5 @@
 import requests,  json
-import threading, time
+import threading
 import logging
 import numpy    as np
 import psycopg2 as pg
@@ -9,13 +9,81 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state   import State, StatesGroup
 from aiogram.dispatcher                 import FSMContext
 from aiogram.types                      import ParseMode, KeyboardButton, ReplyKeyboardMarkup
+from datetime                           import date, timedelta
 
-API_KEY = "61HMKMI43NOMJFEO"
+API_KEY           = "61HMKMI43NOMJFEO"
+ticker            = threading.Event()
+WAIT_TIME_SECONDS = 60 * 60 * 24
+
 TOKEN = "6165848339:AAE2sRqeBVZ7ss23Kw9J9zIx-0I7I5mWu5c"
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level = logging.INFO)
 
 bot = Bot(token = TOKEN)
 dp  = Dispatcher(bot, storage = MemoryStorage())
+
+# Функция для подключения к базе данных
+def connect_db():
+    conn = pg.connect(user     = 'postgres',
+                      password = 'postgres',
+                      host     = 'localhost',
+                      port     = '5432',
+                      database = 'RGZ-RPP')
+    return conn
+
+def period_recalculate():
+    while not ticker.wait(WAIT_TIME_SECONDS):
+        recalculate_stocks()
+
+
+async def add_stock_bd(user_id, stock_name):
+    averages = get_values(stock_name)
+    pg.cursor.execute("""select *
+                           from stock
+                     where user_id = %s
+                       and stock_name = %s""",
+                    (user_id, stock_name,))
+    user = pg.cursor.fetchall()
+    if len(user) == 0:
+        pg.cursor.execute("""insert into stock (user_id, stock_name, averages)
+                                  values (%s, %s, %s)""",
+                                         (user_id, stock_name, averages,))
+        pg.conn.commit()
+        return f'Ценная бумага {stock_name} добавлена к отслеживаемым'
+    else:
+        pg.cursor.execute("""update stock
+                                set averages   = %s
+                              where user_id    = %s
+                                and stock_name = %s""",
+                                (averages, user_id, stock_name,))
+        pg.conn.commit()
+        return f'Ценная бумага {stock_name} обновлена'
+
+def select_stocks(name):
+    pg.cursor.execute("""select stock_name,
+                                entry,
+                                eject
+                           from stock
+                          where stock_name = %s""", (name,))
+    stocks = pg.cursor.fetchall()
+    msg    = ''
+    for stock_name, entry, eject in stocks:
+        if entry == 'null' or eject == 'null':
+            msg += f'Для ценной бумаги {stock_name} не найдено значений :('
+        else:
+            msg += f'Акция {stock_name} имеет оптимальную точку входа: {entry} оптимальную точку выхода: {eject}'
+    return msg
+
+
+async def recalculate_stocks():
+    pg.cursor.execute("""select * from stock""")
+    stocks = pg.cursor.fetchall()
+    for _, user_id, stock_name, entry, eject in stocks:
+        averages = get_values(stock_name)
+        pg.cursor.execute("""update stock
+                                set averages   = %s
+                              where user_id    = %s
+                                and stock_name = %s""",
+                                (averages, user_id, stock_name,))
 
 # Создание списка кнопок
 buttons = [
@@ -32,15 +100,6 @@ keyboard = ReplyKeyboardMarkup(
     one_time_keyboard = True
 )
 
-# Функция для подключения к базе данных
-def connect_db():
-    conn = pg.connect(user     = 'postgres',
-                      password = 'postgres',
-                      host     = 'localhost',
-                      port     = '5432',
-                      database = 'RGZ-RPP')
-    return conn
-
 class States(StatesGroup):
     add_security = State()
 
@@ -48,13 +107,13 @@ class States(StatesGroup):
 # Обработчик для команды /start
 @dp.message_handler(commands = ['start'])
 async def start_handler(message: types.Message):
-    await message.answer( """Привет! Я бот для расчета ценных бумаг. Для начала, выбери действия ниже.
-                             Действия должны быть в порядке:
-                             1. /start,
-                             2. Кнопка 'Добавить ценную бумагу',
-                             3. Кнопка 'Показатели ценных бумаг',
-                             4. Кнопка 'Медиана цены закрытия'!""")
-    await message.answer("Выберите действие:", reply_markup=keyboard)
+    await message.answer("""Привет! Я бот для расчета ценных бумаг. Для начала, выбери действия ниже.
+                            Действия должны быть в порядке:
+                            1. /start,
+                            2. Кнопка 'Добавить ценную бумагу',
+                            3. Кнопка 'Показатели ценных бумаг',
+                            4. Кнопка 'Медиана цены закрытия'!""")
+    await message.answer("Выберите действие:", reply_markup = keyboard)
 
 
 # Обработчик для команды /help
@@ -92,86 +151,81 @@ async def save_security(message: types.Message, state: FSMContext):
 # Обработчик для команды "Показатели ценных бумаг"
 @dp.message_handler(lambda message: message.text == 'Показатели ценных бумаг')
 async def show_indicators(message: types.Message):
-    # Получаем показатели из базы данных для данного пользователя
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute("select security_name from securities where user_id = %s", (message.from_user.id,))
-    securities = cursor.fetchall()
-    conn.close()
+    msg = await select_stocks(message.text)
+    await message.answer(msg)
 
-    if securities:
-        # Обрабатываем каждую ценную бумагу
-        for security in securities:
-            security_name = security[0]
-            # Получаем показатели для ценной бумаги из базы данных
-            conn   = connect_db()
-            cursor = conn.cursor()
-            cursor.execute("select indicator_value from indicators where security_name = %s",
-                           (security_name,))
-            indicators = cursor.fetchall()
-            conn.close()
-
-            if indicators:
-                # Отправляем показатели пользователю
-                await message.answer(f"Показатели для ценной бумаги {security_name}:", reply_markup=keyboard)
-                for indicator in indicators:
-                    await message.answer(f"- {indicator[0]}")
-            else:
-                await message.answer(f"Для ценной бумаги {security_name} не найдено значений!")
-    else:
-        await message.answer("У вас нет отслеживаемых ценных бумаг. Проверьте их наличие в Базе данных!")
-
-
-# Определение показателя доходности ценной бумаги
 def calculate_and_store_data(security_name):
-    url          = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={security_name}&apikey={API_KEY}"
-    response     = requests.get(url)
-    data         = json.loads(response.text)
+    url      = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={security_name}&apikey={API_KEY}"
+    response = requests.get(url)
+    return json.loads(response.text)
 
-    series       = data['Time Series (Daily)']
-    close_prices = []
+def get_values(security_name):
+    data = calculate_and_store_data(security_name)
 
-    for date, values in series.items():
-        close_price = float(values['4. close'])
-        close_prices.append(close_price)
+    # Если компания не найдена, возвращаем налы
+    if data.get('Error Message'):
+        return 'null', 'null'
+    
+    # Дней в периоде
+    maximum_days = 30
 
-    returns         = np.diff(close_prices) / close_prices[:-1]
-    indicator_value = np.mean(returns)
+    # Длина периода для рассчета среднего
+    n = 30
 
-    return indicator_value
+    # Сумма ценностей в текущий период
+    total = 0
 
+    # Счетчик дней в текущий период
+    days_period = 0
 
-def task():
-    while True:
-        # Получаем список ценных бумаг из базы данных
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("select security_name from securities")
-        securities = cursor.fetchall()
-        conn.close()
+    # Счетчик отступа дней от сегодня
+    day_offset = 0
 
-        if securities:
-            for security in securities:
-                security_name = security[0]
+    # Счетчик дней, для которых удалось получить данные
+    days_counted = 0
 
-                # Вызываем функцию для расчета показателя доходности
-                indicator_value = calculate_and_store_data(security_name)
+    # Здесь храним значения по дням
+    vals = []
 
-                # Сохраняем показатель доходности в базу данных
-                conn = connect_db()
-                cursor = conn.cursor()
-                cursor.execute("insert into indicators (security_name, indicator_value) values (%s, %s)",
-                               (security_name, indicator_value,))
-                conn.commit()
-                conn.close()
+    # Массив с датами
+    dates = []
 
-        # Приостанавливаем выполнение на 1 день
-        time.sleep(24 * 60 * 60)
+    # Здесь храним средние значения по периодам длиной n
+    avgs = []
 
+    while days_counted < maximum_days:
+        day = (date.today() - timedelta(days = day_offset)).isoformat()
+        day_info = data['Time Series (Daily)'].get(day)
+        day_offset = day_offset + 1
 
-# Запуск задачи в отдельном потоке
-thread = threading.Thread(target = task)
-thread.start()
+        # Пропускаем день, если по нему нет данных
+        if day_info is None:
+            continue
+
+        dates.append(day)
+
+        # Достаем значение ценности бумаги
+        val = float(day_info['4. close'])
+        vals.append(val)
+        total = total + val
+        days_period = days_period + 1
+
+        # Если до текущего момента была подсчитана сумма по следующим 30 дням,
+        # добавляем среднее значение в массив avg и обнуляем соответствующие переменные
+        if days_period == n:
+            avg = round(total / n, 2)
+            avgs.append(avg)
+            days_period = 0
+            total = 0
+
+        days_counted = days_counted + 1
+
+    # Переворачиваем массивы, так как заполняли их от текущей даты к прошлой
+    vals.reverse()
+    avgs.reverse()
+    dates.reverse()
+
+    return avgs
 
 
 # Обработчик для команды "Медиана цены закрытия"
@@ -193,5 +247,5 @@ async def median_handler(message: types.Message):
 
 
 if __name__ == '__main__':
-    thread = threading.Thread(target = task)
     executor.start_polling(dp, skip_updates = True)
+    period_recalculate()
--- РГР/скрипт создания таблиц.sql
+++ РГР/скрипт создания таблиц.sql
@@ -7,3 +7,10 @@ create table securities(
 	user_id       serial primary key,
 	security_name varchar(255)
 );
+
+create table stock(
+	id         serial primary key,
+	user_id    integer not null,
+	stock_name varchar(10) not null,
+	averages   varchar(20)
+);
