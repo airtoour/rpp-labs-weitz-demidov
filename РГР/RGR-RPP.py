47bed58dfb02f4a0855551f2fdccfc2a4abaed17
airtoour <artur.gips@mail.ru>
2023-05-25 23:39:24 +0700

    установил пайчарм, не помогло...

--- РГР/RGR-RPP.py
+++ РГР/RGR-RPP.py
@@ -3,29 +3,30 @@ import requests
 import json
 import threading
 import aiogram
-import numpy    as np
+import numpy as np
 import psycopg2 as pg
 
-from aiogram                            import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-API_KEY           = "61HMKMI43NOMJFEO"
-TOKEN             = "6165848339:AAE2sRqeBVZ7ss23Kw9J9zIx-0I7I5mWu5c"
+API_KEY = "61HMKMI43NOMJFEO"
+TOKEN = "6165848339:AAE2sRqeBVZ7ss23Kw9J9zIx-0I7I5mWu5c"
 WAIT_TIME_SECONDS = 300
 
 ticker = threading.Event()
 
 # Создание бота и диспетчера
-bot = Bot(token = TOKEN)
-dp = Dispatcher(bot, storage = MemoryStorage())
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 # Подключение к базе данных Postgres
-conn = pg.connect(database = "RGZ-RPP",
-                  user     = "postgres",
-                  password = "postgres",
-                  host     = "localhost",
-                  port     = "5432")
+conn = pg.connect(database="RGZ-RPP",
+                  user="postgres",
+                  password="postgres",
+                  host="localhost",
+                  port="5432")
 cur = conn.cursor()
+
 
 # Функция для получения данных из API
 def get_stock_data(symbol):
@@ -33,6 +34,7 @@ def get_stock_data(symbol):
     response = requests.get(url)
     data = json.loads(response.text)
     return data
+
 
 # Функция расчета медианного значения цены закрытия за период анализа
 def calculate_median(data):
@@ -42,6 +44,7 @@ def calculate_median(data):
         close_prices.append(float(values["4. close"]))
     median = np.median(close_prices)
     return median
+
 
 # Функция обработки команды "Добавить ценную бумагу к портфелю"
 async def add_stock(message: types.Message):
@@ -55,6 +58,7 @@ async def add_stock(message: types.Message):
     conn.commit()
 
     await message.answer(f"Ценная бумага {stock_name} добавлена к отслеживаемым!")
+
 
 # Функция обработки команды "Показатели отслеживаемых ценных бумаг"
 async def show_indicators(message: types.Message):
@@ -70,6 +74,7 @@ async def show_indicators(message: types.Message):
         else:
             await message.answer(f"Для ценной бумаги {stock_name} не найдено значений :(")
 
+
 # Функция определения и сохранения показателя доходности ценной бумаги
 def calculate_and_store_data():
     cur.execute("select security_name from securities")
@@ -83,14 +88,17 @@ def calculate_and_store_data():
             cur.execute("insert into indicators(security_name, indicator_value) values (%s, %s)", (stock_name, median))
             conn.commit()
 
+
 # Регистрация обработчиков команд
-dp.register_message_handler(add_stock,       commands = "add_stock")
-dp.register_message_handler(show_indicators, commands = "show_indicators")
+dp.register_message_handler(add_stock, commands="add_stock")
+dp.register_message_handler(show_indicators, commands="show_indicators")
+
 
 # Запуск периодической задачи для расчета показателя доходности
 def periodic_task():
     while not ticker.wait(WAIT_TIME_SECONDS):
         calculate_and_store_data()
+
 
 # Запуск бота и периодической задачи
 if __name__ == "__main__":
