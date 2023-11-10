f0da0761b04455e007de78ace9280d2f3f890696
airtoour <artur.gips@mail.ru>
2023-11-10 21:02:15 +0700

    поправил красиво

---
+++ СЕМ2/Лаб3/routes/main.py
@@ -0,0 +1,14 @@
+from flask               import Flask
+from routes.region_route import region
+from routes.car_route    import cars
+from routes.area_route   import area_route
+
+app = Flask(__name__)
+
+# Регистрация Blueprint
+app.register_blueprint(region)
+app.register_blueprint(cars)
+app.register_blueprint(area_route)
+
+if __name__ == '__main__':
+    app.run(debug = True)
