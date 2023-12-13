2815658a354c94733ff04467abf998726bac3108
airtoour <artur.gips@mail.ru>
2023-12-13 23:09:27 +0700

    и эта готова рил

---
+++ СЕМ2/RGZ_dobrovv/config.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/SEM2RGR-Dobrov'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'astanin'
