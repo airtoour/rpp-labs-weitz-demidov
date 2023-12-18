04fcf673c97e3adb9393e3746fd5f370e65d3e89
airtoour <artur.gips@mail.ru>
2023-12-18 19:51:43 +0700

    доделаю еще

--- СЕМ2/RGZ_rudnevva/models.py
+++ СЕМ2/RGZ_rudnevva/models.py
@@ -1,56 +1,52 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import (IntegerField, FloatField, StringField, EmailField,
-                     PasswordField, DateField, SubmitField, SelectField, validators)
-from wtforms.validators import DataRequired, InputRequired, Length
-
-db = SQLAlchemy()
+                     PasswordField, DateField, SubmitField, SelectField)
+from wtforms.validators import DataRequired, InputRequired, Length, Email
+from config import db
 
 class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer,     primary_key=True)
-    name = db.Column(db.String(60),  nullable=False, unique=True)
-    email = db.Column(db.String(60),  nullable=False, unique=True)
+    id  = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), nullable=False, unique=True)
+    email = db.Column(db.String(60), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
 
-    # Добавляем сразу 2 функции для паролей, чтобы не использовать их явно при регистрации пользователя.
-    # Облегчает это жизнь.
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
-
 class RegistrationForm(FlaskForm):
-    name     = StringField('Имя пользователя: ', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired(), Length(min=6, max=32)])
-    email    = EmailField('Эл. почта', validators=[DataRequired(), validators.Email()])
-    submit   = SubmitField('Регистрация')
+    name = StringField('Имя пользователя: ', validators=[DataRequired()])
+    password = PasswordField('Пароль: ', validators=[DataRequired(), Length(min=6, max=32)])
+    email = EmailField('Электронная почта: ', validators=[DataRequired(), Email()])
 
 class LoginForm(FlaskForm):
-    name     = StringField('Имя пользователя: ', validators=[DataRequired()])
-    password = PasswordField('Пароль: ', validators=[InputRequired()])
-    submit   = SubmitField('Войти')
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[InputRequired()])
 
+class OperationDb(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    operation_type = db.Column(db.String(12), nullable=False)
+    operation_amount = db.Column(db.Float, nullable=False)
+    operation_date = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-class OperationAddDb(db.Model):
-    id        = db.Column(db.Integer, primary_key=True)
-    oper_type = db.Column(db.String(12), nullable=False)
-    amount    = db.Column(db.Float, nullable=False)
-    oper_date = db.Column(db.DateTime, nullable=False)
-    user_id   = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+class Operation(FlaskForm):
+    id = IntegerField('Номер операции: ', validators=[DataRequired()])
+    operation_type = SelectField('Тип операции: ', choices=[(1, "Доход"), (2, "Расход")], validators=[DataRequired()])
+    operation_amount = FloatField('Сумма: ', validators=[DataRequired()])
+    operation_date = DateField('Дата операции: ', validators=[DataRequired()])
+    user_id = IntegerField('Номер пользователя: ', validators=[DataRequired()])
+    submit = SubmitField('Добавить операцию')
 
-class OperationAddForm(FlaskForm):
-    id        = IntegerField('Номер операции:', validators=[DataRequired()])
-    oper_type = SelectField('Тип операции:', choices=[(1, "Доход"), (2, "Расход")], validators=[DataRequired()])
-    amount    = FloatField('Сумма:', validators=[DataRequired()])
-    oper_date = DateField('Дата операции:', validators=[DataRequired()])
-    user_id   = IntegerField('Номер пользователя:', validators=[DataRequired()])
-    submit    = SubmitField('Добавить операцию')
+class OperationDelete(FlaskForm):
+    id = IntegerField('Номер операции: ')
+    submit = SubmitField('Удалить операцию')
 
-class OperationFindForm(FlaskForm):
-    fromDate = DateField('Начало: ', validators=[DataRequired()])
-    endDate  = DateField('Конец: ', validators=[DataRequired()])
-    submit   = SubmitField('Найти')
+class OperationForm(FlaskForm):
+    from_date = DateField('Начало: ', validators=[DataRequired()])
+    end_date = DateField('Конец: ', validators=[DataRequired()])
+    submit = SubmitField('Найти')
--- СЕМ2/RGZ_rudnevva/routes.py
+++ СЕМ2/RGZ_rudnevva/routes.py
@@ -1,17 +1,12 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login       import LoginManager, login_user, current_user, login_required, logout_user
-from config            import app, db
-from models            import (Users, OperationFindForm, OperationAddDb, OperationAddForm,
-                               LoginForm, RegistrationForm)
-from datetime          import datetime, timedelta
-from flask_babel       import Babel, _
-
-babel = Babel(app)
-babel.init_app(app, default_locale='ru')
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user
+from config import app, db
+from models import Users, OperationFindForm, OperationDb, Operation, OperationDelete
+from datetime import datetime, timedelta
 
 operation = Blueprint('operation', __name__, template_folder='templates')
 
-login_manager            = LoginManager(app)
+login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
 @login_manager.user_loader
@@ -21,32 +16,28 @@ def load_user(name):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    err_message = None
-
-    if form.validate_on_submit():
-        name = form.name.data
-        password = form.password.data
+    if request.method == 'POST':
+        name = request.form.get('name')
+        password = request.form.get('password')
 
         user = Users.query.filter_by(name=name).first()
 
-        if user.check_password(password):
+        if user and user.check_password(password):
             login_user(user)
-            return redirect(url_for('operation.view_operation', lang='ru'))
+            return redirect(url_for('operation.view_operation'))
         else:
-            err_message = 'Неправильно введен пароль или логин, попробуйте ещё раз.'
+            err_message = 'Такого пользователя не существует или введен неверный пароль!'
+            return render_template('login.html', err_message=err_message)
 
-    return render_template('login.html', form=form, err_message=err_message)
+    return render_template('login.html')
 
 
 @app.route('/registration', methods=['GET', 'POST'])
-def signup():
-    form = RegistrationForm()
-
-    if form.validate_on_submit():
-        name     = form.name.data
-        email    = form.email.data
-        password = form.password.data
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
         is_user = Users.query.filter_by(email=email).first()
 
@@ -57,81 +48,104 @@ def signup():
             try:
                 new_user = Users(name=name, email=email)
                 new_user.set_password(password)
-
                 db.session.add(new_user)
                 db.session.commit()
 
                 login_user(new_user)
 
-                return redirect(url_for('operation.view_operation', lang='ru'))
+                return redirect(url_for('operation.view_operation'))
             except Exception as e:
                 db.session.rollback()
+                app.logger.error(f"Произошла ошибка: {e}")
                 err_message = 'Произошла какая-то ошибка!'
-                app.logger.error(str(e))
+
                 return render_template('registration.html', err_message=err_message)
 
-    return render_template('registration.html', form=form)
+    return render_template('registration.html')
 
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    err_message = 'Вы вышли из аккаунта'
+    return render_template('login.html', err_message=err_message)
 
 
-@operation.route('/<lang>', methods=['GET', 'POST'])
+@operation.route('/', methods=['GET', 'POST'])
 @login_required
-def view_operation(lang='ru'):
-    if lang == 'en':
-        form = OperationFormEn()
-        form.fromDate.label.text = _('Start Date')
-        form.endDate.label.text = _('End Date')
-        form.submit.label.text = _('Find')
-    else:
-        form = OperationFormRu()
+def view_operation():
+    form = OperationFindForm()
 
     if form.validate_on_submit():
-        from_date = form.fromDate.data
-        end_date = form.endDate.data
+        from_date = form.from_date.data
+        end_date = form.end_date.data
     else:
         end_date = datetime.utcnow()
         from_date = end_date - timedelta(days=30)
 
-    operations = OperationAddDb.query.filter_by(
-        user_id=current_user.id).filter(OperationAddDb.oper_date.between(from_date, end_date)).all()
+    operations = OperationDb.query.filter_by(
+        user_id=current_user.id).filter(OperationDb.operation_date.between(from_date, end_date)).all()
 
-    total_income  = sum(op.amount for op in operations if op.amount >= 0)
-    total_expense = sum(op.amount for op in operations if op.amount < 0)
+    total_income  = sum(op.operation_amount for op in operations if op.operation_amount >= 0)
+    total_expense = sum(op.operation_amount for op in operations if op.operation_amount < 0)
 
-    return render_template(f'{lang}-operation.html', form=form, operations=operations,
+    return render_template('operation.html', form=form, operations=operations,
                            total_income=total_income, total_expense=total_expense)
 
 @operation.route('/add', methods=['GET', 'POST'])
 @login_required
 def add_operation():
-    form = OperationAdd()
+    form = Operation()
 
     if form.validate_on_submit():
-        oper_id   = form.id.data
-        oper_type = form.oper_type.data
-        amount    = form.amount.data
-        date      = form.oper_date.data
-        user_id   = form.user_id.data
+        oper_id = form.id.data
+        oper_type = form.operation_type.data
+        amount = form.operation_amount.data
+        date = form.operation_date.data
+        user_id = form.user_id.data
 
-        if OperationAddDb.query.filter_by(id=oper_id).first():
-            message = f'Операция с таким ID: {oper_id} уже существует!'
+        if OperationDb.query.filter_by(id=oper_id).first():
+            message = f'ID: {oper_id}. Такая операция уже существует!'
         else:
             try:
-                operations = OperationAddDb(id=oper_id, oper_type=oper_type, amount=amount, oper_date=date, user_id=user_id)
+                operations = OperationDb(id=oper_id, operation_type=oper_type, operation_amount=amount,
+                                         operation_date=date, user_id=user_id)
                 db.session.add(operations)
                 db.session.commit()
-                message = f'Операция {oper_id} успешно добавлена!'
+                message = f'Операция на сумму {amount} успешно добавлена!'
             except Exception as e:
                 db.session.rollback()
-                app.logger.error(f'Error during operation addition: {str(e)}')
+                app.logger.error(f"Произошла ошибка: {e}")
                 message = 'Произошла ошибка во время добавления!'
 
-        return render_template('add_operation.html', form=form, message=message)
+        return render_template('add-operation.html', form=form, message=message)
 
-    return render_template('add_operation.html', form=form)
+    return render_template('add-operation.html', form=form)
+
+@operation.route('/delete-operation', methods=['GET', 'POST'])
+@login_required
+def delete_operation():
+    form = OperationDelete()
+    message = None
+
+    if form.validate_on_submit():
+        oper_id = form.id.data
+
+        operations = OperationDb.query.filter_by(id=oper_id).first()
+        if not operations:
+            message = 'Такой операции не существует :('
+        else:
+            if operations.oper_type or operations.amount or operations.date or operations.user_id:
+                operations.oper_type = oper_type
+                operations.amount = amount
+                operations.date = date
+                operations.user_id = user_id
+
+                db.session.commit()
+
+                message = 'Операция успешно обновлена!'
+            else:
+                message = 'Вы не выбрали ни одного поля для заполнения.'
+
+    return render_template('change-operation.html', form=form, message=message)
