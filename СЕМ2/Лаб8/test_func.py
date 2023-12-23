96aaa87d4a8f53ae6fe8484cb1ee49205eb61506
airtoour <artur.gips@mail.ru>
2023-12-23 10:28:45 +0700

    че

--- СЕМ2/Лаб8/test_func.py
+++ СЕМ2/Лаб8/test_func.py
@@ -3,7 +3,7 @@ from triangle_func import get_triangle_type, IncorrectTriangleSides
 
 class TestGetTriangleType(unittest.TestCase):
     def test_equilateral_triangle(self):
-        self.assertEqual(get_triangle_type(3, 4, 3), "equilateral")
+        self.assertEqual(get_triangle_type(3, 3, 3), "equilateral")
 
     def test_isosceles_triangle(self):
         self.assertEqual(get_triangle_type(5, 5, 6), "isosceles")
@@ -13,7 +13,7 @@ class TestGetTriangleType(unittest.TestCase):
 
     def test_incorrect_triangle_sides(self):
         with self.assertRaises(IncorrectTriangleSides):
-            get_triangle_type(20, 2, 2)
+            get_triangle_type(0, 0, 0)
 
 
 if __name__ == '__main__':
